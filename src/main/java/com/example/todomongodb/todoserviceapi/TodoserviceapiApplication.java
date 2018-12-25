67f82c04e5f0c42e4c638fea285f090fa6284a40
HarishmaA <harishma.arivazhagan@anywhere.co>
2018-12-25 12:27:53 +0530

    removed unnecessary codes

--- src/main/java/com/example/todomongodb/todoserviceapi/TodoserviceapiApplication.java
+++ src/main/java/com/example/todomongodb/todoserviceapi/TodoserviceapiApplication.java
@@ -1,43 +1,20 @@
 package com.example.todomongodb.todoserviceapi;
 
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 
 
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
+
+
 @SpringBootApplication
 public class TodoserviceapiApplication {
 	
-    
-
+  
 	public static void main(String[] args) {
 		SpringApplication.run(TodoserviceapiApplication.class, args);
 	}
 	
- //       @Bean
-//	    CommandLineRunner init(DomainRepository domainRepository) {
-//
-//	        return args -> {
-//
-//	            Domain obj1 = domainRepository.findFirstByDomain("mkyong.com");
-//	            System.out.println("Hi, find by domain" +obj1);
-//	           // Document canvas = new Document("item", "canvas")
-//	                 //   .append("qty", 100));
-//	             
-//
-//	        };
-//
-//	    }
 
 }
 
